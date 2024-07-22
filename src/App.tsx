d9e5f2c40185c0d18e2004bdf3dd582437031418
Svaym <Vaymeraa22@mail.ru>
2024-07-22 19:42:36 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl font-bold">
+          asdasdasdas
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
