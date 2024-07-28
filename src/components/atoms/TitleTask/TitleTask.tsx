8f395ecf6b7b597acae748e2a520653bd144a4ec
Svaym <162036192+Svaym@users.noreply.github.com>
2024-07-28 09:52:38 +0500

    Update TitleTask.tsx

--- src/components/atoms/TitleTask/TitleTask.tsx
+++ src/components/atoms/TitleTask/TitleTask.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 interface TitleTaskProps {
   children: string;
   onClick: () => void;
+  textColor?: string
 }
-export default function TitleTask({ children, onClick }: TitleTaskProps) {
+export default function TitleTask({ children, onClick, textColor }: TitleTaskProps) {
   return (
     <h2
       onClick={onClick}
-      className="text-lg font-medium truncate cursor-pointer transition-colors duration-300 ease-linear hover:text-gray-400 "
+      className={`text-lg font-medium truncate cursor-pointer ${textColor} transition-colors duration-300 ease-linear hover:text-gray-400`}
     >
       {children}
     </h2>
   );
 }
+
